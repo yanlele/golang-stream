95d7e2117c3b7ac490ca1650c41c045ea556cd63
yanlele <331393627@qq.com>
2019-07-16 09:24:15 +0800

    [+]add login

--- api/handlers.go
+++ api/handlers.go
@@ -9,3 +9,9 @@ import (
 func CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	_, _ = io.WriteString(w, "Create User Handler")
 }
+
+func Login(w http.ResponseWriter, r *http.Request, p httprouter.Params)  {
+	username := p.ByName("user_name")
+	_, _ = io.WriteString(w, username)
+
+}
--- api/main.go
+++ api/main.go
@@ -8,6 +8,8 @@ import (
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
 	router.POST("/user", CreateUser)
+	router.POST("/user/:user_name", Login)
+
 	return router
 }
 
