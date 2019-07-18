be2b803668a85c71aa0da35cf2b5dd0a3e257abe
yanlele <331393627@qq.com>
2019-07-18 08:59:36 +0800

    [+]deops

---
+++ api/dbops/api.go
@@ -0,0 +1,17 @@
+package dbops
+
+import (
+	"database/sql"
+)
+
+func OpenConn() *sql.DB {
+
+}
+
+func AddUserCredential(loginName string, pwd string) error {
+
+}
+
+func GetUserGredential(loginName string) (string, error) {
+
+}
---
+++ api/dbops/response.go
@@ -0,0 +1,13 @@
+package dbops
+
+import (
+	"net/http"
+)
+
+func SendErrorResponse(w http.ResponseWriter) {
+
+}
+
+func SendNormalResponse(w http.ResponseWriter) {
+
+}
