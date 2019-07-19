0fb0247c31e3e2a218f524bc2f78705d9aacbc67
yanlele <331393627@qq.com>
2019-07-19 09:15:56 +0800

    [+]add api

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,14 +1,30 @@
 package dbops
 
 import (
-	"database/sql"
+	"log"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func AddUserCredential(loginName string, pwd string) error {
-
+	stmtIns, err := dbConn.Prepare("insert into user (login_name, pwd) values (?, ?)")
+	if err != nil {
+		return err
+	}
+	stmtIns.Exec(loginName, pwd)
+	stmtIns.Close()
+	return nil
 }
 
 func GetUserGredential(loginName string) (string, error) {
+	stmtOut, err := dbConn.Prepare("select pwd from user where login_name = ?")
+	if err != nil {
+		log.Printf("%s", err)
+		return "", err
+	}
 
+	var pwd string
+	stmtOut.QueryRow(loginName).Scan(&pwd)
+	stmtOut.Close()
+
+	return pwd, nil
 }
