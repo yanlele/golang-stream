1287345daa4f9750037960cf9fd73125f592cf8b
yanlele <331393627@qq.com>
2019-07-19 09:22:56 +0800

    [-]delete user

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,8 +1,8 @@
 package dbops
 
 import (
-	"log"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 func AddUserCredential(loginName string, pwd string) error {
@@ -10,8 +10,8 @@ func AddUserCredential(loginName string, pwd string) error {
 	if err != nil {
 		return err
 	}
-	stmtIns.Exec(loginName, pwd)
-	stmtIns.Close()
+	_, _ = stmtIns.Exec(loginName, pwd)
+	_ = stmtIns.Close()
 	return nil
 }
 
@@ -23,8 +23,20 @@ func GetUserGredential(loginName string) (string, error) {
 	}
 
 	var pwd string
-	stmtOut.QueryRow(loginName).Scan(&pwd)
-	stmtOut.Close()
+
+	// 只查询一行
+	_ = stmtOut.QueryRow(loginName).Scan(&pwd)
+	_ = stmtOut.Close()
 
 	return pwd, nil
+}
+
+func DeleteUser(loginName string, pwd string) error {
+	stmtDel, err := dbConn.Prepare("delete from user where login_name = ?, pwd = ?")
+	if err != nil {
+		log.Printf("DeleteUser error: %s", err)
+	}
+	_, _ = stmtDel.Exec(loginName, pwd)
+	_ = stmtDel.Close()
+	return nil
 }
