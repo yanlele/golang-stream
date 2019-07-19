fa66b691087b0c85317203430e22d700a9ef6bf3
yanlele <331393627@qq.com>
2019-07-19 23:31:39 +0800

    [+]add test

--- api/dbops/api_test.go
+++ api/dbops/api_test.go
@@ -5,3 +5,34 @@ import (
 )
 
 // init(dblogin, truncate tables) -> run tests -> clear data(truncate tables)
+
+func ClearTables() {
+	dbConn.Exec("truncate user")
+	dbConn.Exec("truncate video_info")
+	dbConn.Exec("truncate comments")
+	dbConn.Exec("truncate sessions")
+}
+
+func TestMain(m *testing.M)  {
+
+}
+
+func TestUserWorkFlow(t *testing.T) {
+
+}
+
+func TestAddUser(t *testing.T) {
+
+}
+
+func TestGerUser(t *testing.T) {
+
+}
+
+func TestDeleteUser(t *testing.T) {
+
+}
+
+func TestReGetUser(t *testing.T)  {
+
+}
